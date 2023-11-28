fac6ce7d6051ef6426fd0ff37454dc883be07912
Miguel Lopes <miguel.lopes@estudiants.urv.cat>
2023-11-28 12:49:04 +0100

    :merge

---
+++ src/java/model/entities/Customer.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+/**
+ *
+ * @author mlopes
+ */
+@Entity
+@Table(name = "Customer")
+@XmlRootElement
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/java/service/CostumerService.java
+++ src/java/service/CostumerService.java
@@ -18,18 +18,18 @@ import jakarta.persistence.Query;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import model.entities.Comment;
-import model.entities.Costumer;
+import model.entities.Customer;
 import model.entities.Game;
 
 @Stateless
 @Path("costumer")
-public class CostumerService extends AbstractFacade<Costumer> {
+public class CostumerService extends AbstractFacade<Customer> {
 
     @PersistenceContext(unitName = "Homework1PU")
     private EntityManager em;
 
     public CostumerService() {
-        super(Costumer.class);
+        super(Customer.class);
     }
 
     @Override
@@ -40,15 +40,30 @@ public class CostumerService extends AbstractFacade<Costumer> {
     @GET
     @Override
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Costumer> findAll() {
+    public List<Customer> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Costumer findById(@PathParam("id") int id) {
-        return em.find(Costumer.class, id);
+    public Customer findById(@PathParam("id") int id) {
+        return em.find(Customer.class, id);
+    }
+
+    @PUT
+    @Path("/prova/{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response updateCostumer(@PathParam("id") Long id, Customer user) {
+        Customer userToUpdate = em.find(Customer.class, id);
+        if (userToUpdate == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        userToUpdate.setName(user.getName());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setPassword(user.getPassword());
+        em.persist(userToUpdate);
+        return Response.ok().build();
     }
 
 }
