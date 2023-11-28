d0483c1a89e67261e57e86cbbe14c9f86d41d7d9
Pere Escoda <pere.escoda@estudiants.urv.cat>
2023-11-28 12:57:11 +0100

    :merge

--- src/java/model/entities/User.java
+++ src/java/model/entities/User.java
@@ -4,10 +4,9 @@
  */
 package model.entities;
 
-import jakarta.persistence.Entity;
 import java.io.Serializable;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  *
@@ -17,6 +16,50 @@ import javax.persistence.Table;
 @Table(name = "User")
 public class User implements Serializable{
 
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
--- src/java/service/UserService.java
+++ src/java/service/UserService.java
@@ -14,8 +14,14 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import authn.Secured;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
+import java.util.Comparator;
+import model.entities.Comment;
 import model.entities.User;
+
 
 @Stateless
 @Path("user")
@@ -33,5 +39,39 @@ public class UserService extends AbstractFacade<User> {
         return em;
     }
     
-
+    @GET
+    @Secured
+    @Path("/prova")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Override
+    public List<User> findAll() {
+        return em.createQuery("SELECT new User(u.id, u.nom, u.email) FROM User u", User.class)
+                .getResultList();
+    }
+    
+    @GET
+    @Path("/prova/{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getUserById(@PathParam("id") Long id) {
+        User user = em.find(User.class, id);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(user).build();
+    }
+    
+    @PUT
+    @Path("/prova/{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response updateUser(@PathParam("id") Long id, User user) {
+        User userToUpdate = em.find(User.class, id);
+        if (userToUpdate == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        userToUpdate.setName(user.getName());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setPassword(user.getPassword());
+        em.persist(userToUpdate);
+        return Response.ok().build();
+    }
 }
